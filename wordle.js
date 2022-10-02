ec8a2912bf46bf377302ddf14d574ac11341b3e9
leungsekyu <384252134@qq.com>
2022-10-02 16:28:29 +0800

    refactored

--- wordle.js
+++ wordle.js
@@ -1,282 +1,270 @@
-// header
 let header = document.querySelector('header');
-// row
-let currRow = document.querySelectorAll('.row')[0];
-let rowQty = 6;
-let idx = 0;
-// state
-let isWin = false;
-// API
-let fetchWordURL = 'https://words.dev-apis.com/word-of-the-day?random=1';
-let validateWordURL = ' https://words.dev-apis.com/validate-word';
-// reference word
+let currLine = document.querySelectorAll('.line')[0];
+let keyboard = document.querySelector('.keyboard');
+
+let lineQty = 6;
+let lineInx = 0;
+
+let wordLength = 5;
+let wordInx = 0;
+
 let ref = '';
+let fetchWordURL = 'https://words.dev-apis.com/word-of-the-day';
+let validateWordURL = ' https://words.dev-apis.com/validate-word';
+
+let isWin = false;
 
 init();
 
-// work flow
 async function init() {
-  await fetchWord();
-  initAllRows();
-  currRowAddEventListeners();
+  ref = await fetchWord();
+  document.addEventListener('keydown', solveKeydown);
+  keyboard.addEventListener('click', solveClick);
 }
 
-function moveToNextRow() {
-  idx++;
-  if (idx < rowQty) {
-    currRow = document.querySelectorAll('.row')[idx];
-    initCurrRow();
-    currRowAddEventListeners();
+/* event handler */
+function solveKeydown(event) {
+  let currLetter = currLine.children[wordInx];
+  let keyVal = event.key;
+  if (isLetter(keyVal)) {
+    solveLetter(currLetter, keyVal);
+  } else if (keyVal === 'Backspace') {
+    solveBackspace(currLetter);
+  } else if (keyVal === 'Enter') {
+    solveEnter(currLetter);
+  }
+}
+
+function solveClick(event) {
+  if (event.target.tagName === 'BUTTON') {
+    let currLetter = currLine.children[wordInx];
+    let keyVal = event.target.innerText;
+    if (isLetter(keyVal)) {
+      solveLetter(currLetter, keyVal);
+    } else if (keyVal === '←') {
+      solveBackspace(currLetter);
+    } else if (keyVal === 'Enter') {
+      solveEnter(currLetter);
+    }
+  }
+}
+
+// event handler assistant functions
+function solveLetter(currLetter, keyVal) {
+  if (currLetter.innerText === '') {
+    currLetter.innerText = keyVal;
+    currLetter.classList.add('disabled');
+    if (wordInx < wordLength - 1) {
+      wordInx++;
+    }
+  } else {
+    if (wordInx < wordLength - 1) {
+      wordInx++;
+      currLetter = currLine.children[wordInx];
+      currLetter.innerText = keyVal;
+      currLetter.classList.add('disabled');
+    }
+  }
+}
+
+function solveBackspace(currLetter) {
+  if (currLetter.innerText !== '') {
+    currLetter.innerText = '';
+    currLetter.classList.remove('disabled');
+    if (wordInx > 0) {
+      wordInx--;
+    }
+  } else {
+    if (wordInx > 0) {
+      wordInx--;
+    }
+    currLetter = currLine.children[wordInx];
+    currLetter.innerText = '';
+    currLetter.classList.remove('disabled');
+  }
+}
+
+async function solveEnter(currLetter) {
+  if (wordInx === wordLength - 1) {
+    if (currLetter.innerText !== '') {
+      await validateCurrLine(currLine);
+    }
+  }
+}
+
+/* validation */
+async function validateCurrLine(currLine) {
+  let currWord = getCurrWord(currLine);
+  fireSpinner(currLine);
+  let isValid = await validateCurrWord(currWord);
+  stopSpinner(currLine);
+  if (isValid) {
+    renderCurrLine(currLine, currWord);
+    if (currWord == ref) {
+      isWin = true;
+      solveWin();
+    }
+    if (!isWin) {
+      moveToNextLine();
+    }
+  } else {
+    fireShaker(currLine);
+    setTimeout(() => {
+      stopShaker(currLine);
+    }, 1000);
+  }
+}
+
+function moveToNextLine() {
+  if (lineInx < lineQty - 1) {
+    lineInx++;
+    currLine = document.querySelectorAll('.line')[lineInx];
+    wordInx = 0;
   } else {
     solveLose();
   }
 }
 
-// initialization
-function initAllRows() {
-  let allInputs = document.querySelectorAll('input');
-  allInputs.forEach((currInput) => {
-    currInput.disabled = true;
-  });
-  focusBeginning();
-}
-
-function initCurrRow() {
-  let inputs = Array.from(currRow.children);
-  inputs.forEach((currInput) => {
-    currInput.value = '';
-    currInput.classList.remove('disabled');
-  });
-  focusBeginning();
-}
-
-function focusBeginning() {
-  currRow.children[0].disabled = false;
-  currRow.children[0].focus();
-}
-
-// evnet listeners
-function currRowAddEventListeners() {
-  currRow.addEventListener('keydown', solveAlpha);
-  currRow.addEventListener('input', solveInput);
-  // backspace solution:
-  // 1. 'keydown' event fires, cursor goes back to previous input
-  // 2. 'input' event fires, delete 1 letter in previous input
-  currRow.addEventListener('keydown', solveBackspace);
-  currRow.addEventListener('keydown', solveEnter);
-}
-
-function currRowRemoveEventListeners() {
-  currRow.removeEventListener('keydown', solveAlpha);
-  currRow.removeEventListener('input', solveInput);
-  currRow.removeEventListener('keydown', solveBackspace);
-  currRow.removeEventListener('keydown', solveEnter);
-}
-
-// event handles
-function solveAlpha(event) {
-  if (!isLetter(event.key)) {
-    if (event.key !== 'Backspace' && event.key !== 'Enter') {
-      event.preventDefault();
-    }
-  }
-}
-
-function solveInput(event) {
-  let currInput = event.target;
-  let currMaxLength = parseInt(currInput.maxLength, 10);
-  let currLength = currInput.value.length;
-
-  if (currLength === currMaxLength) {
-    let nextInput = currInput.nextElementSibling;
-    if (nextInput !== null) {
-      nextInput.disabled = false;
-      currInput.disabled = true;
-      nextInput.focus();
-    }
-    currInput.classList.add('disabled');
-  }
-}
-
-function solveBackspace(event) {
-  let currInput = event.target;
-  if (event.key === 'Backspace') {
-    if (currInput.value.length === 0) {
-      let prevInput = currInput.previousElementSibling;
-      if (prevInput !== null) {
-        prevInput.disabled = false;
-        prevInput.classList.remove('disabled');
-        prevInput.focus();
-      }
-    } else {
-      currInput.classList.remove('disabled');
-    }
-  }
-}
-
-async function solveEnter(event) {
-  let currInput = event.target;
-  if (event.key === 'Enter') {
-    let nextInput = currInput.nextElementSibling;
-    if (currInput.value !== '' && nextInput === null) {
-      await validateCurrRow(currInput);
-    }
-  }
-}
-
-/* validation flow */
-async function validateCurrRow(currInput) {
-  let currWord = mergeWord(currRow);
-  fireSpinner(currRow);
-  let isValidWord = await validateWord(currWord);
-  stopSpinner(currRow);
-
-  if (isValidWord) {
-    currInput.disabled = true;
-    renderCurrRow(currRow, currWord);
-    if (currWord === ref) {
-      solveWin();
-    }
-    currRowRemoveEventListeners();
-    if (!isWin) {
-      moveToNextRow();
-    }
-  }
-}
-
-function renderCurrRow(row, word) {
-  let inputs = Array.from(row.children);
-  let refCounter = countWord(ref);
-  let counter = countWord(word);
-
-  Object.keys(counter).forEach((char) => {
-    if (char in refCounter) {
-      solveExact(char, inputs, refCounter, counter);
-      solveExist(char, inputs, refCounter, counter);
-    } else {
-      solveNonExist(char, inputs, counter);
-    }
-  });
-}
-
 /* word */
-async function fetchWord() {
-  let response = await fetch(fetchWordURL);
-  let responseJSON = await response.json();
-  ref = responseJSON.word;
-}
-
-function mergeWord(row) {
-  let inputs = Array.from(row.children);
-  let word = '';
-  inputs.forEach((input) => {
-    word += input.value;
-  });
-  return word.toLowerCase();
-}
-
-function countWord(word) {
-  let counter = {};
-  let chars = word.split('');
-  chars.forEach((char, idx) => {
-    if (!(char in counter)) {
-      counter[char] = { quantity: 1 };
-      counter[char].position = [idx];
-      counter[char].matched = [];
-      counter[char].mismatched = [];
-    } else {
-      counter[char].quantity++;
-      counter[char].position.push(idx);
-    }
-  });
-  return counter;
-}
-
-/* validation assistant */
 function isLetter(letter) {
   return /^[a-zA-Z]$/.test(letter);
 }
 
-async function validateWord(word) {
+async function fetchWord() {
+  let response = await fetch(fetchWordURL);
+  let responseJSON = await response.json();
+  let word = responseJSON.word;
+  return word;
+}
+
+function getCurrWord(currLine) {
+  let currLetters = Array.from(currLine.children);
+  let currWord = '';
+  currLetters.forEach((currLetter) => (currWord += currLetter.innerText));
+  return currWord.toLowerCase();
+}
+
+async function validateCurrWord(currWord) {
   let response = await fetch(validateWordURL, {
     method: 'POST',
-    body: JSON.stringify({ word: word }),
+    body: JSON.stringify({ word: currWord }),
   });
   let responseJSON = await response.json();
-  return responseJSON.validWord;
+  let isValid = responseJSON.validWord;
+  return isValid;
 }
 
-function solveExact(char, inputs, refCounter, counter) {
-  counter[char].position.forEach((currPos) => {
-    if (refCounter[char].position.includes(currPos)) {
-      counter[char].matched.push(currPos);
-      inputs[currPos].classList.add('exact');
-      let guessedCharIndicator = document.getElementById(char);
-      guessedCharIndicator.classList.add('exact');
+function countWord(word) {
+  let wordCounter = {};
+  let letters = word.split('');
+  letters.forEach((letter, idx) => {
+    if (!(letter in wordCounter)) {
+      wordCounter[letter] = { quantity: 1 };
+      wordCounter[letter].position = [idx];
+      wordCounter[letter].matched = [];
+      wordCounter[letter].mismatched = [];
     } else {
-      counter[char].mismatched.push(currPos);
+      wordCounter[letter].quantity++;
+      wordCounter[letter].position.push(idx);
+    }
+  });
+  return wordCounter;
+}
+
+/* rendering */
+function renderCurrLine(currLine, word) {
+  let currLetters = Array.from(currLine.children);
+  let refCounter = countWord(ref);
+  let wordCounter = countWord(word);
+
+  Object.keys(wordCounter).forEach((currLetter) => {
+    if (currLetter in refCounter) {
+      solveExact(currLetter, currLetters, refCounter, wordCounter);
+      solveExist(currLetter, currLetters, refCounter, wordCounter);
+    } else {
+      solveNonExist(currLetter, currLetters, wordCounter);
     }
   });
 }
 
-function solveExist(char, inputs, refCounter, counter) {
-  let refNum = refCounter[char].position.length;
-  let matchedNum = counter[char].matched.length;
-  let mismatchedNum = counter[char].mismatched.length;
-  if (refNum - matchedNum === 0) {
-    counter[char].mismatched.forEach((currPos) => {
-      inputs[currPos].classList.add('non-exist');
+function solveExact(currLetter, currLetters, refCounter, wordCounter) {
+  wordCounter[currLetter].position.forEach((currPos) => {
+    if (refCounter[currLetter].position.includes(currPos)) {
+      wordCounter[currLetter].matched.push(currPos);
+      currLetters[currPos].classList.add('exact');
+      let correspondBtn = document.getElementById(currLetter);
+      correspondBtn.classList.add('exact');
+    } else {
+      wordCounter[currLetter].mismatched.push(currPos);
+    }
+  });
+}
+
+function solveExist(currLetter, currLetters, refCounter, wordCounter) {
+  let refQty = refCounter[currLetter].position.length;
+  let matchedQty = wordCounter[currLetter].matched.length;
+  let mismatchedQty = wordCounter[currLetter].mismatched.length;
+  if (refQty - matchedQty === 0) {
+    wordCounter[currLetter].mismatched.forEach((currPos) => {
+      currLetters[currPos].classList.add('non-exist');
     });
   }
-  if (refNum - matchedNum >= mismatchedNum) {
-    for (let i = 0; i < mismatchedNum; i++) {
-      let currPos = counter[char].mismatched[i];
-      inputs[currPos].classList.add('exist');
+  if (refQty - matchedQty >= mismatchedQty) {
+    for (let i = 0; i < mismatchedQty; i++) {
+      let currPos = wordCounter[currLetter].mismatched[i];
+      currLetters[currPos].classList.add('exist');
     }
   }
-  if (refNum - matchedNum < mismatchedNum) {
-    for (let i = 0; i < mismatchedNum; i++) {
-      let currPos = counter[char].mismatched[i];
-      if (i < refNum - matchedNum) {
-        inputs[currPos].classList.add('exist');
+  if (refQty - matchedQty < mismatchedQty) {
+    for (let i = 0; i < mismatchedQty; i++) {
+      let currPos = wordCounter[currLetter].mismatched[i];
+      if (i < refQty - matchedQty) {
+        currLetters[currPos].classList.add('exist');
       } else {
-        inputs[currPos].classList.add('non-exist');
+        currLetters[currPos].classList.add('non-exist');
       }
     }
   }
-  let guessedCharIndicator = document.getElementById(char);
-  if (!guessedCharIndicator.classList.contains('exact')) {
-    guessedCharIndicator.classList.add('exist');
+  let correspondBtn = document.getElementById(currLetter);
+  if (!correspondBtn.classList.contains('exact')) {
+    correspondBtn.classList.add('exist');
   }
 }
 
-function solveNonExist(char, inputs, counter) {
-  counter[char].position.forEach((currPos) => {
-    inputs[currPos].classList.add('non-exist');
-    let guessedCharIndicator = document.getElementById(char);
-    guessedCharIndicator.classList.add('non-exist');
+function solveNonExist(currLetter, currLetters, wordCounter) {
+  wordCounter[currLetter].position.forEach((currPos) => {
+    currLetters[currPos].classList.add('non-exist');
+    let correspondBtn = document.getElementById(currLetter);
+    correspondBtn.classList.add('non-exist');
   });
 }
 
 /* animation */
 // spinner
-function fireSpinner(row) {
-  let inputs = Array.from(row.children);
-  inputs.forEach((input) => {
-    input.classList.add('spin');
+function fireSpinner(currLine) {
+  let currLetters = Array.from(currLine.children);
+  currLetters.forEach((currLetter) => {
+    currLetter.classList.add('spin');
   });
 }
 
-function stopSpinner(row) {
-  let inputs = Array.from(row.children);
-  inputs.forEach((input) => {
-    input.classList.remove('spin');
+function stopSpinner(currLine) {
+  let currLetters = Array.from(currLine.children);
+  currLetters.forEach((currLetter) => {
+    currLetter.classList.remove('spin');
   });
+}
+
+function fireShaker(currLine) {
+  currLine.classList.add('shake');
+}
+
+function stopShaker(currLine) {
+  currLine.classList.remove('shake');
 }
 
 // result
 function solveWin() {
-  isWin = true;
   header.innerText = '🎊';
 }
 
